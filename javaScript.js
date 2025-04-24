36e1349e547366290ed93f655523ae7547047e0b
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-04-24 21:16:58 +0700

    Adding logic to calclator to make it work

--- javaScript.js
+++ javaScript.js
@@ -1,75 +1,103 @@
-/**
- * calculator 
+let firstNumber = "";
+let secondNumber = "";
+let operatorUsed = "";
 
-- add
-- subtract
-- multiply
-- divide
+const numberButton = document.querySelectorAll("#number-button");
+const operatorButton = document.querySelectorAll("#operator-button");
+const clearButton = document.querySelector("#clear-button");
 
-For example, 3 + 5. Create three variables, one for each part of the operation. You’ll use these variables to update your display later.
-
-Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
-
-Create a basic HTML calculator with buttons for each digit and operator (including =).
-There should also be a display for the calculator
-Add a “clear” button.
-
-Create the functions that populate the display when you click the digit buttons. 
-You should store the content of the display (the number) in a variable for use in the next step
-
-You’ll need to store the first and second numbers input by the user and then operate() on them when the user presses the = button,
-according to the operator that was selected between the numbers.
-You should already have the code that can populate the display, so once operate has been called, update the display with the result of the operation.
-You need to figure out how to store all the values and call the operate function with them.
- */
-
-let firstNumber = 0;
-let secondNumber = 0;
-let operation;
-
-const buttons = document.querySelectorAll("button");
 const display = document.querySelector("#display");
 
-function add(firstNumber, secondNumber) {
-  return firstNumber + secondNumber;
+function add(a, b) {
+  return a + b;
 }
-function subtract(firstNumber, secondNumber) {
-  return firstNumber - secondNumber;
+function subtract(a, b) {
+  return a - b;
 }
-function multiply(firstNumber, secondNumber) {
-  return firstNumber * secondNumber;
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  if (b === 0) {
+    return "ERROR"    
+  }
+  return parseFloat((a / b).toFixed(4));
 }
 
-function divide(firstNumber, secondNumber) {
-  return firstNumber / secondNumber;
-}
-
-function operate(firstNumber, secondNumber, operation) {
-  if (operation === "+") {
-    return add(firstNumber, secondNumber);
-  }
-  if (operation === "-") {
-    return subtract(firstNumber, secondNumber);
-  }
-  if (operation === "*") {
-    return multiply(firstNumber, secondNumber);
-  }
-  if (operation === "/") {
-    return devide(firstNumber, secondNumber);
+function operate(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+    default:
+      break;
   }
 }
+function handleNumber() {
+  numberButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      if (display.textContent === "ERROR") {
+        display.textContent = button.textContent;
+        firstNumber = button.textContent;
+        secondNumber = "";
+        operatorUsed = "";
+        return;
+      }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (button.id === "clear") {
-      return (display.textContent = "0");
-    }
-    if (display.innerHTML == "0" && !isNaN(button.id)) {
-      return (display.textContent = button.id);
-    } else if (isNaN(display.textContent.slice(-1)) && isNaN(button.id)) {
-      return (display.textContent = display.textContent.slice(0, -1) + button.id);
-    } else {
-      display.textContent += button.id;
-    }
+      if (operatorUsed === "") {
+        firstNumber += button.textContent;
+      } else {
+        secondNumber += button.textContent;
+      }
+      if (display.textContent === "0") {
+        display.textContent = button.textContent;
+      } else {
+        display.textContent += button.textContent;
+      }
+    });
   });
-});
+}
+function handleOperator() {
+  operatorButton.forEach((button) => {
+    button.addEventListener("click", () => {   
+      const lastChar = display.textContent.slice(-1);
+
+      if (button.textContent === "=" && operatorUsed === "") {                 
+        return
+      }
+      if (button.textContent === "=") {    
+        const calcResult = operate(operatorUsed, firstNumber, secondNumber);
+        firstNumber = calcResult;
+        secondNumber = 0;
+        operatorUsed = "";
+        return display.textContent = calcResult;
+      }       
+      if (isNaN(lastChar)) {
+        operatorUsed = button.textContent;
+        return (display.textContent = display.textContent.slice(0, -1) + button.textContent);
+      }
+      operatorUsed = button.textContent;
+      display.textContent += button.textContent;           
+    });
+  });
+}
+function clearDisplay() {
+  clearButton.addEventListener("click", () => {
+    display.textContent = "0";
+    firstNumber = 0;
+    secondNumber = 0;
+    operatorUsed = ""
+  });
+}
+
+handleNumber();
+handleOperator();
+clearDisplay();
+
